b687857bb9f3791ea9ced1c55d3d86c29d3526f3
Rauf138484 <raufking1384@gmail.com>
2025-01-16 20:59:50 +0330

    Add files via upload

---
+++ Rauofproject.com/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+Console.Write("Enter the array lenghts: ");
+int n= int.Parse(Console.ReadLine());
+int[] myArray = new int[n];
+Console.WriteLine("Enter the elements of the array: ");
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("Element {0}:  " ,i+1);
+    myArray[i] = int.Parse(Console.ReadLine());         
+}
+Console.WriteLine("The elements are: ");
+foreach (int i in myArray)
+{
+    Console.Write(i + " ");
+}
